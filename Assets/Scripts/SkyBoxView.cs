d68dc3b4d821bed66a3dddd18243d247a43c568d
Philip Unger <pdotunger@gmail.com>
2018-08-04 17:47:33 +0200

    frequency changes

---
+++ Assets/Scripts/SkyBoxView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxView : MonoBehaviour
+{
+    public Material skyBox;
+
+    private Material _fallbackSkybox;
+
+    void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ExposureControl(float amount)
+    {
+        skyBox.SetFloat("_Exposure", amount);
+    }
+
+    void Init()
+    {
+        GatherDefaultData();
+    }
+
+    void GatherDefaultData()
+    {
+        _fallbackSkybox = new Material(skyBox);
+    }
+}
