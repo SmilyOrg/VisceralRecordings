03096fa6d7a5fba02624d8fc2f8838e5e83801ba
Philip Unger <pdotunger@gmail.com>
2018-08-05 09:29:59 +0200

    aded audio file changes from carlos

---
+++ Assets/Scripts/AudioHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AudioHelpers
+{
+    //public static float[] spectrum = new float[64];
+
+    //public static float[] ApplyAudioSourceFrequency(float[] spectrum)
+    //{
+    //    mainTrack.GetSpectrumData(spectrum, 0, FFTWindow.Triangle);
+
+    //    for (int i = 0; i < createdObjects.Length; i++)
+    //    {
+    //        var specVal = _spectrum[i];
+    //        var go = createdObjects[i];
+
+    //        go.transform.DOShakeScale(durationToSample, Vector3.up * specVal * punchIntensity);
+    //    }
+    //}
+}
